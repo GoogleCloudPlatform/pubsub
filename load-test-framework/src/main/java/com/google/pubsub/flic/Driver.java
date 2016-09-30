f900a88482e1a4172c7de62336d8c87be2d93962
Max Dietz <maxdietz@google.com>
2016-09-30 12:08:40 -0400

    Started work on Driver.
    Work remaining:
    
    1. Fix flags in Driver
    2. Add gRPC Command servers on clients
    3. Actually start and run clients

--- load-test-framework/src/main/java/com/google/pubsub/flic/Driver.java
+++ load-test-framework/src/main/java/com/google/pubsub/flic/Driver.java
@@ -20,27 +20,23 @@ import com.google.pubsub.flic.argumentparsing.BaseArguments;
 import com.google.pubsub.flic.argumentparsing.CPSArguments;
 import com.google.pubsub.flic.argumentparsing.CompareArguments;
 import com.google.pubsub.flic.argumentparsing.KafkaArguments;
-import com.google.pubsub.flic.common.Utils;
-import com.google.pubsub.flic.cps.CPSPublishingTask;
-import com.google.pubsub.flic.cps.CPSRoundRobinPublisher;
-import com.google.pubsub.flic.cps.CPSRoundRobinSubscriber;
-import com.google.pubsub.flic.cps.CPSSubscribingTask;
-import com.google.pubsub.flic.kafka.KafkaConsumerTask;
-import com.google.pubsub.flic.kafka.KafkaPublishingTask;
+import com.google.pubsub.flic.controllers.Client.ClientType;
+import com.google.pubsub.flic.controllers.GCEController;
 import com.google.pubsub.flic.processing.Comparison;
-import com.google.pubsub.flic.processing.MessageProcessingHandler;
 import com.google.pubsub.flic.task.TaskArgs;
-import java.io.File;
-import java.util.logging.LogManager;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** Drives the execution of the framework through command line arguments. */
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.logging.LogManager;
+
+/**
+ * Drives the execution of the framework through command line arguments.
+ */
 public class Driver {
 
-  private static final Logger log = LoggerFactory.getLogger(Driver.class.getName());
+  private static final Logger log = LoggerFactory.getLogger(Driver.class);
 
   public static void main(String[] args) {
     // Turns off all java.util.logging.
@@ -51,7 +47,7 @@ public class Driver {
       CPSArguments cpsArgs = new CPSArguments();
       KafkaArguments kafkaArgs = new KafkaArguments();
       CompareArguments dataComparisonArgs = new CompareArguments();
-      JCommander jCommander = new JCommander(baseArgs);
+      JCommander jCommander = new JCommander(BaseArguments.class);
       jCommander.addCommand(CPSArguments.COMMAND, cpsArgs);
       jCommander.addCommand(KafkaArguments.COMMAND, kafkaArgs);
       jCommander.addCommand(CompareArguments.COMMAND, dataComparisonArgs);
@@ -59,75 +55,26 @@ public class Driver {
       if (jCommander.getParsedCommand() == null) {
         if (baseArgs.isHelp()) {
           jCommander.usage();
+          return;
         }
-        return;
+        System.exit(1);
       }
+      // Compares data dumps for correctness.
       if (jCommander.getParsedCommand().equals(CompareArguments.COMMAND)) {
-        // Compares data dumps for correctness.
         Comparison c = new Comparison(dataComparisonArgs.getFile1(), dataComparisonArgs.getFile2());
         c.compare();
         return;
       }
-      // Use the builder to construct custom arguments for each task.
-      TaskArgs.TaskArgsBuilder builder =
-          new TaskArgs.TaskArgsBuilder()
-              .numMessages(baseArgs.getNumMessages())
-              .topics(baseArgs.getTopics());
-      TaskArgs taskArgs;
-      if (jCommander.getParsedCommand().equals(CPSArguments.COMMAND)) {
-        // The "cps" command was invoked.
-        MessageProcessingHandler cpsHandler =
-            new MessageProcessingHandler(baseArgs.getNumMessages());
-        builder =
-            builder
-                .cpsProject(cpsArgs.getProject())
-                .numResponseThreads(cpsArgs.getNumResponseThreads())
-                .rateLimit(cpsArgs.getRateLimit());
-        if (baseArgs.isPublish()) {
-          // Create a task which publishes to CPS.
-          taskArgs =
-              builder
-                  .messageSize(baseArgs.getMessageSize())
-                  .batchSize(cpsArgs.getBatchSize())
-                  .build();
-          cpsHandler.setLatencyType(MessageProcessingHandler.LatencyType.PUB_TO_ACK);
-          CPSRoundRobinPublisher publisher = new CPSRoundRobinPublisher(cpsArgs.getNumClients());
-          log.info("Creating a task which publishes to CPS.");
-          new CPSPublishingTask(taskArgs, publisher, cpsHandler).execute();
-        } else {
-          // Create a task which consumes from CPS.
-          if (baseArgs.isDumpData()) {
-            cpsHandler.setFiledump(new File(Utils.CPS_FILEDUMP_PATH));
-          }
-          taskArgs = builder.build();
-          cpsHandler.setLatencyType(MessageProcessingHandler.LatencyType.END_TO_END);
-          CPSRoundRobinSubscriber subscriber = new CPSRoundRobinSubscriber(cpsArgs.getNumClients());
-          log.info("Creating a task which consumes from CPS.");
-          new CPSSubscribingTask(taskArgs, subscriber, cpsHandler).execute();
-        }
-      } else {
-        // The "kafka" command was invoked.
-        MessageProcessingHandler bundle = new MessageProcessingHandler(baseArgs.getNumMessages());
-        builder = builder.broker(kafkaArgs.getBroker());
-        if (baseArgs.isPublish()) {
-          // Create a task that publishes to Kafka.
-          taskArgs = builder.messageSize(baseArgs.getMessageSize()).build();
-          KafkaProducer<String, String> publisher =
-              KafkaPublishingTask.getInitializedProducer(taskArgs);
-          log.info("Creating a task which publishes to Kafka");
-          new KafkaPublishingTask(taskArgs, publisher).execute();
-        } else {
-          // Create a task that consumes from Kafka.
-          if (baseArgs.isDumpData()) {
-            bundle.setFiledump(new File(Utils.KAFKA_FILEDUMP_PATH));
-          }
-          taskArgs = builder.broker(kafkaArgs.getBroker()).build();
-          KafkaConsumer<String, String> consumer =
-              KafkaConsumerTask.getInitializedConsumer(taskArgs);
-          log.info("Creating a task which consumes from Kafka.");
-          new KafkaConsumerTask(taskArgs, consumer, bundle).execute();
-        }
+
+      Map<ClientType, Integer> clientTypes = baseArgs.getClientTypes();
+      if (clientTypes.values().stream().allMatch((n) -> n == 0)) {
+        jCommander.usage();
+        return;
       }
+
+      TaskArgs taskArgs;
+      GCEController controller = GCEController.newGCEController(baseArgs.getProject(),
+          clientTypes, Executors.newCachedThreadPool());
     } catch (Exception e) {
       log.error("An error occurred...", e);
       System.exit(1);
