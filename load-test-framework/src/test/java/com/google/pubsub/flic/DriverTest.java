e62f6d5a6dee6e9a2c5847b12bbda9ef0b944e7d
Max Dietz <maxdietz@google.com>
2016-10-13 17:35:56 -0400

    Adds a test for the ParameterValidator

---
+++ load-test-framework/src/test/java/com/google/pubsub/flic/DriverTest.java
@@ -0,0 +1,50 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+package com.google.pubsub.flic;
+
+import com.beust.jcommander.ParameterException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for {@link Driver}.
+ */
+public class DriverTest {
+  @Test
+  public void testGreaterThanZeroValidator() {
+    Driver.GreaterThanZeroValidator validator = new Driver.GreaterThanZeroValidator();
+    validator.validate("test", "1");
+    try {
+      validator.validate("test", "-1");
+      assertTrue(false);
+    } catch (ParameterException e) {
+      // we expect this to throw since -1 < 1
+    }
+    try {
+      validator.validate("test", "0");
+      assertTrue(false);
+    } catch (ParameterException e) {
+      // we expect this to throw since 0 < 1
+    }
+    try {
+      validator.validate("test", "abc");
+      assertTrue(false);
+    } catch (ParameterException e) {
+      // we expect this to throw since abc is not a number
+    }
+  }
+}
