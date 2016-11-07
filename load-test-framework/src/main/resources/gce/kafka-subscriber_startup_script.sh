45e67eaf4e3034418ab8ace1e80d231de222618f
John Heizelman <jrheizelman@google.com>
2016-11-07 11:35:01 -0800

    Added apt-get update to scripts to fix java 8 install failure

--- load-test-framework/src/main/resources/gce/kafka-subscriber_startup_script.sh
+++ load-test-framework/src/main/resources/gce/kafka-subscriber_startup_script.sh
@@ -4,6 +4,7 @@ readonly TMP="$(mktemp -d)"
 [[ "${TMP}" != "" ]] || error mktemp failed
 
 # Download the loadtest binary to this machine and install Java 8.
+/usr/bin/apt-get update
 /usr/bin/apt-get install -y openjdk-8-jre-headless & PIDAPT=$!
 /usr/bin/gsutil cp "gs://cloud-pubsub-loadtest/driver.jar" "${TMP}"
 
