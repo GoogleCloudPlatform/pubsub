04396cae075646d0e06eb5af5bedd1ec44336979
Amanda Chalfant <aechalf@google.com>
2017-02-23 14:33:09 -0800

    Add details to simplified producer

--- pubsub-mapped-api/src/main/java/com/google/pubsub/clients/producer/PubsubProducerConfig.java
+++ pubsub-mapped-api/src/main/java/com/google/pubsub/clients/producer/PubsubProducerConfig.java
@@ -63,7 +63,8 @@ public class PubsubProducerConfig extends AbstractConfig {
             .define(MAX_REQUEST_SIZE_CONFIG, Type.INT, DEFAULT_MAX_REQUEST_SIZE, atLeast(0), Importance.MEDIUM, MAX_REQUEST_SIZE_DOC)
             .define(BATCH_SIZE_CONFIG, Type.INT, 1, Importance.MEDIUM, BATCH_SIZE_DOC)
             .define(ACKS_CONFIG, Type.STRING, "1", Importance.MEDIUM, ACKS_DOC)
-            .define(PROJECT_CONFIG, Type.STRING, Importance.HIGH, PROJECT_DOC);
+            .define(PROJECT_CONFIG, Type.STRING, Importance.HIGH, PROJECT_DOC)
+            .define(MAX_REQUEST_SIZE_CONFIG, Type.INT, 1*1024*1024, atLeast(0), Importance.MEDIUM, MAX_REQUEST_SIZE_DOC);
   }
 
   PubsubProducerConfig(Map<?, ?> properties) {
