1616cf3169d9b8b44192edc9e38f8b91fd713578
Amanda Chalfant <aechalf@google.com>
2017-03-01 09:52:17 -0800

    Working on unit tests, need to mock the publisher calls

--- pubsub-mapped-api/src/test/java/com/google/pubsub/clients/producer/PubsubProducerTest.java
+++ pubsub-mapped-api/src/test/java/com/google/pubsub/clients/producer/PubsubProducerTest.java
@@ -14,30 +14,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*package com.google.kafka.clients.producer;
+package com.google.pubsub.clients.producer;
 
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.common.KafkaException;
-import org.apache.kafka.common.network.Selectable;
+import com.google.common.collect.ImmutableMap;
+import java.util.StringTokenizer;
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.test.MockMetricsReporter;
-import org.apache.kafka.test.MockProducerInterceptor;
-import org.apache.kafka.test.MockSerializer;
+import org.apache.kafka.common.config.ConfigException;
+
+
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@RunWith(PowerMockRunner.class)
-@PowerMockIgnore("javax.management.*")
 public class PubsubProducerTest {
 
+  private static final String TOPIC = "testTopic";
+  private static final String MESSAGE = "testMessage";
+
+  /* Constructor Tests */
   @Test
   public void testConstructorWithSerializers() {
     Properties props = new Properties();
@@ -46,11 +47,39 @@ public class PubsubProducerTest {
   }
 
   @Test(expected = ConfigException.class)
-  public void testNoSerializerProvided() {
+  public void testConstructorNoSerializerProvided() {
     Properties props = new Properties();
-    props.put(PubsubProducerConfig.PROJECT_CONFIG, "dataproc-kafka-test");
-    new PubsubProducer(props);
+    props.setProperty(PubsubProducerConfig.PROJECT_CONFIG, "dataproc-kafka-test");
+    new PubsubProducer(props).close();
   }
 
+  @Test(expected = ConfigException.class)
+  public void testConstructorNoProjectProvided() {
+    Properties props = new Properties();
+    props.putAll(new ImmutableMap.Builder<>()
+        .put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        .put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        .build()
+    );
+    new PubsubProducer(props).close();
+  }
 
-}*/
+  /* send() tests */
+ /* @Test(expected = RuntimeException.class)
+  public void testSendPublisherClosed() {
+
+  }*/
+
+  private PubsubProducer getNewProducer() {
+    Properties props = new Properties();
+    props.putAll(new ImmutableMap.Builder<>()
+        .put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        .put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        .put("project", "dataproc-kafka-test")
+        .build()
+    );
+
+    return new PubsubProducer(props);
+  }
+
+}
