6d14d11af5e931d2f36124e9aa1b2e17f32b51d5
Paulina Pietrzyk <pietrzykp@google.com>
2017-07-26 11:45:19 -0700

    Basic implementation of consumer methods

--- pubsub-mapped-api/src/main/java/com/google/pubsub/clients/consumer/KafkaConsumer.java
+++ pubsub-mapped-api/src/main/java/com/google/pubsub/clients/consumer/KafkaConsumer.java
@@ -16,18 +16,35 @@
 
 package com.google.pubsub.clients.consumer;
 
+import com.google.pubsub.common.ChannelUtil;
+import com.google.pubsub.kafkastubs.TopicPartition;
+import com.google.pubsub.kafkastubs.common.Metric;
+import com.google.pubsub.kafkastubs.common.MetricName;
+import com.google.pubsub.kafkastubs.common.PartitionInfo;
+import com.google.pubsub.kafkastubs.common.record.TimestampType;
+import com.google.pubsub.kafkastubs.common.serializatiom.Deserializer;
+import com.google.pubsub.kafkastubs.consumer.*;
+import com.google.pubsub.kafkastubs.consumer.ConsumerRecords;
+import com.google.pubsub.v1.DeleteSubscriptionRequest;
+import com.google.pubsub.v1.ListTopicsRequest;
+import com.google.pubsub.v1.ListTopicsResponse;
+import com.google.pubsub.v1.PublisherGrpc;
+import com.google.pubsub.v1.PublisherGrpc.PublisherBlockingStub;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.PullRequest;
+import com.google.pubsub.v1.PullResponse;
+import com.google.pubsub.v1.ReceivedMessage;
+import com.google.pubsub.v1.SubscriberGrpc.SubscriberBlockingStub;
+import com.google.pubsub.v1.Subscription;
+
+import com.google.pubsub.v1.Topic;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
 import org.apache.commons.lang3.NotImplementedException;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
-import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
-import org.apache.kafka.clients.consumer.OffsetCommitCallback;
-import org.apache.kafka.common.Metric;
-import org.apache.kafka.common.MetricName;
-import org.apache.kafka.common.PartitionInfo;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.Deserializer;
+
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -35,11 +52,35 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
+import com.google.pubsub.v1.SubscriberGrpc;
 
 public class KafkaConsumer<K, V> implements Consumer<K, V> {
 
-  public KafkaConsumer(Map<String, Object> configs) {
+  private static String GOOGLE_CLOUD_PROJECT;
+  private static String SUBSCRIPTION_PREFIX;
+  private static String TOPIC_PREFIX;
+  private static int DEFAULT_PARTITION = 0;
 
+  private ChannelUtil channelUtil;
+  private SubscriberBlockingStub subscriberBlockingStub;
+  private PublisherBlockingStub publisherBlockingStub;
+  private Map<String, Subscription> subscriptions = new HashMap<>();
+
+  Deserializer<K> keyDeserializer;
+  Deserializer<V> valueDeserializer;
+
+  static {
+    GOOGLE_CLOUD_PROJECT = "projects/" + System.getenv("GOOGLE_CLOUD_PROJECT");
+    SUBSCRIPTION_PREFIX = GOOGLE_CLOUD_PROJECT + "/subscriptions/";
+    TOPIC_PREFIX = GOOGLE_CLOUD_PROJECT + "/topics/";
+  }
+
+  public KafkaConsumer(Map<String, Object> configs) {
+    this.channelUtil = new ChannelUtil();
+    this.subscriberBlockingStub = SubscriberGrpc.newBlockingStub(channelUtil.getChannel())
+        .withCallCredentials(channelUtil.getCallCredentials());
+    this.publisherBlockingStub = PublisherGrpc.newBlockingStub(channelUtil.getChannel())
+        .withCallCredentials(channelUtil.getCallCredentials());
   }
 
   public KafkaConsumer(Map<String, Object> configs,
@@ -63,34 +104,105 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
   }
 
   public Set<String> subscription() {
-    throw new NotImplementedException("Not yet implemented");
+    return subscriptions.keySet();
   }
 
   public void subscribe(Collection<String> topics, ConsumerRebalanceListener listener) {
-    throw new NotImplementedException("Not yet implemented");
+    unsubscribe();
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+    for(String topic: topics) {
+      if(!subscriptions.containsKey(topic)) {
+        Subscription subscription = subscriberBlockingStub.createSubscription(Subscription.newBuilder()
+            .setName(SUBSCRIPTION_PREFIX + topic + "_" + timestamp
+                    .getTime())
+            .setTopic(TOPIC_PREFIX + topic)
+            .build());
+        subscriptions.put(topic, subscription);
+      }
+    }
   }
 
   @Override
   public void subscribe(Collection<String> topics) {
-    throw new NotImplementedException("Not yet implemented");
+    subscribe(topics, null);
   }
 
   @Override
   public void subscribe(Pattern pattern, ConsumerRebalanceListener listener) {
-    throw new NotImplementedException("Not yet implemented");
-  }
+    ListTopicsResponse listTopicsResponse = publisherBlockingStub
+        .listTopics(ListTopicsRequest.newBuilder()
+            .setProject(GOOGLE_CLOUD_PROJECT)
+            .build());
+    int topicsCount = listTopicsResponse.getTopicsCount();
+    List<String> matchingTopics = new ArrayList<>();
 
-  public void unsubscribe() {
-    throw new NotImplementedException("Not yet implemented");
+    for(int i = 0; i < topicsCount; ++i) {
+      Topic topic = listTopicsResponse.getTopics(i);
+      String topicName = topic.getName().substring(TOPIC_PREFIX.length(), topic.getName().length());
+      Matcher m = pattern.matcher(topicName);
+      if(m.matches())
+        matchingTopics.add(topicName);
+    }
+    subscribe(matchingTopics);
   }
 
   @Override
-  public void assign(Collection<TopicPartition> partitions) {
-    throw new NotImplementedException("Not yet implemented");
+  public void unsubscribe() {
+    for(Subscription s: subscriptions.values()) {
+      subscriberBlockingStub.deleteSubscription(DeleteSubscriptionRequest.newBuilder()
+          .setSubscription(s.getName()).build());
+    }
+    subscriptions = new HashMap<>();
   }
 
   @Override
   public ConsumerRecords<K, V> poll(long timeout) {
+    Map<TopicPartition, List<ConsumerRecord<K, V>>> pollRecords = new HashMap<>();
+
+    for(Subscription s: subscriptions.values()) {
+
+      String topic = s.getTopic().substring(TOPIC_PREFIX.length(), s.getTopic().length());
+      TopicPartition partition = new TopicPartition(topic, DEFAULT_PARTITION);
+
+      List<ConsumerRecord<K, V>> subscriptionRecords = new ArrayList<>();
+
+      PullResponse pulled = subscriberBlockingStub.pull(PullRequest.newBuilder()
+          .setSubscription(s.getName())
+          .setMaxMessages(10)
+          .setReturnImmediately(true).build());
+      int receivedMessagesCount = pulled.getReceivedMessagesCount();
+
+      for(int i = 0; i < receivedMessagesCount; ++i) {
+        ReceivedMessage receivedMessage = pulled.getReceivedMessages(i);
+        PubsubMessage message = receivedMessage.getMessage();
+
+        long timestamp = message.getPublishTime().getSeconds();
+        long offset = timestamp;
+        TimestampType timestampType = TimestampType.CREATE_TIME;
+        String key = message.getMessageId();
+
+        int serializedValueSize = message.getData().toByteArray().length;
+        int serializedKeySize = key.getBytes().length;
+
+
+        V value = valueDeserializer.deserialize(topic, message.getData().toByteArray());
+        K key2 = keyDeserializer.deserialize(topic, key.getBytes());
+
+
+        ConsumerRecord<K, V> record = new ConsumerRecord<K, V>(topic, DEFAULT_PARTITION,
+            offset,timestamp,timestampType, 1, serializedKeySize, serializedValueSize, key2, value);
+
+        subscriptionRecords.add(record);
+      }
+      pollRecords.put(partition, subscriptionRecords);
+    }
+
+    return new ConsumerRecords<>(pollRecords);
+  }
+
+  @Override
+  public void assign(Collection<TopicPartition> partitions) {
     throw new NotImplementedException("Not yet implemented");
   }
 
@@ -178,11 +290,12 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
   }
 
   public void close() {
-
+    unsubscribe();
+    channelUtil.closeChannel();
   }
 
   public void close(long timeout, TimeUnit timeUnit) {
-    throw new NotImplementedException("Not yet implemented");
+    close();
   }
 
   public void wakeup() {
