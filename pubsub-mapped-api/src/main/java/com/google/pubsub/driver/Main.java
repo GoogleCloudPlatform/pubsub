084e10f6d8d24ddc445ff719c6348fc09d2fe0e9
gewillovic <gewillovic@google.com>
2017-08-11 17:58:40 -0700

    Deleted some unused dependencies.

--- pubsub-mapped-api/src/main/java/com/google/pubsub/driver/Main.java
+++ pubsub-mapped-api/src/main/java/com/google/pubsub/driver/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
     Properties props = new Properties();
     props.put("project", "dataproc-kafka-test");
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-    final KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+    final KafkaProducer<Integer, String> producer = new KafkaProducer<>(props);
 
     System.out.println("PUBLISHING FROM MAIN THREAD");
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
       producer.send(new ProducerRecord<>(
-          "wow", 0, Integer.toString(i), "message" + Integer.toString(i)));
+          "wow", 0, i, Double.toString(i)));
       System.out.println(Thread.currentThread().getName() + " " + i);
     }
 
@@ -31,9 +31,9 @@ public class Main {
       @Override
       public void run() {
         System.out.println("PUBLISHING FROM ALT THREAD");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
           producer.send(new ProducerRecord<>(
-              "wow1", 0, Integer.toString(i), "message" + Integer.toString(i)));
+              "wow1", 0, i, Double.toString(i)));
           System.out.println(Thread.currentThread().getName() + " " + i);
         }
       }
@@ -43,7 +43,6 @@ public class Main {
       @Override
       public void run() {
         System.out.println("FLUSHING RECORDS");
-        producer.flush();
         while(true);
       }
     }).start();
