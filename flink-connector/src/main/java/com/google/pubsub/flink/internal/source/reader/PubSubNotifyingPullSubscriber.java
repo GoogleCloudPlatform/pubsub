f6c4a486d85953700a5744a6d7e21984049225f3
hannahrogers-google <hannahrogers@google.com>
2023-05-25 19:26:09 +0000

    feat: create new flink source reader classes

---
+++ flink-connector/src/main/java/com/google/pubsub/flink/internal/source/reader/PubSubNotifyingPullSubscriber.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.pubsub.flink.internal.source.reader;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.api.core.ApiService.Listener;
+import com.google.api.core.ApiService.State;
+import com.google.api.core.SettableApiFuture;
+import com.google.cloud.pubsub.v1.AckReplyConsumer;
+import com.google.cloud.pubsub.v1.MessageReceiver;
+import com.google.cloud.pubsub.v1.Subscriber;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.errorprone.annotations.concurrent.GuardedBy;
+import com.google.pubsub.v1.PubsubMessage;
+import java.util.Optional;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PubSubNotifyingPullSubscriber implements NotifyingPullSubscriber {
+  public class SubscriberWakeupException extends Exception {}
+
+  public interface SubscriberFactory {
+    Subscriber create(MessageReceiver receiver);
+  }
+
+  private final Subscriber subscriber;
+
+  @GuardedBy("this")
+  private Optional<Throwable> permanentError = Optional.empty();
+
+  @GuardedBy("this")
+  private Optional<SettableApiFuture<Void>> notification = Optional.empty();
+
+  @GuardedBy("this")
+  private final LinkedBlockingQueue<PubsubMessage> messages = new LinkedBlockingQueue<>();
+
+  private final AckTracker ackTracker;
+
+  public PubSubNotifyingPullSubscriber(SubscriberFactory factory, AckTracker ackTracker) {
+    this.ackTracker = ackTracker;
+    this.subscriber = factory.create(this::receiveMessages);
+    subscriber.addListener(
+        new Listener() {
+          @Override
+          public void failed(State unused, Throwable throwable) {
+            setPermanentError(throwable);
+            completeNotification(Optional.of(throwable));
+          }
+        },
+        MoreExecutors.directExecutor());
+    this.subscriber.startAsync().awaitRunning();
+  }
+
+  @Override
+  public synchronized ApiFuture<Void> notifyDataAvailable() {
+    if (permanentError.isPresent()) {
+      return ApiFutures.immediateFailedFuture(permanentError.get());
+    }
+    if (!messages.isEmpty()) {
+      return ApiFutures.immediateFuture(null);
+    }
+    if (!notification.isPresent()) {
+      notification = Optional.of(SettableApiFuture.create());
+    }
+    return notification.get();
+  }
+
+  @Override
+  public synchronized Optional<PubsubMessage> pullMessage() throws Throwable {
+    if (permanentError.isPresent()) {
+      throw permanentError.get();
+    }
+    if (messages.size() == 0) {
+      return Optional.empty();
+    }
+    return Optional.of(messages.remove());
+  }
+
+  @Override
+  public void interruptNotify() {
+    completeNotification(Optional.of(new SubscriberWakeupException()));
+  }
+
+  @Override
+  public void shutdown() {
+    setPermanentError(new Exception("Subscriber shut down"));
+    subscriber.stopAsync().awaitTerminated();
+  }
+
+  private synchronized void receiveMessages(
+      PubsubMessage message, AckReplyConsumer ackReplyConsumer) {
+    ackTracker.addPendingAck(ackReplyConsumer);
+    messages.add(message);
+    completeNotification(Optional.empty());
+  }
+
+  @VisibleForTesting
+  synchronized void setPermanentError(Throwable t) {
+    permanentError = Optional.of(t);
+  }
+
+  private synchronized void completeNotification(Optional<Throwable> t) {
+    if (notification.isPresent()) {
+      if (t.isPresent()) {
+        notification.get().setException(t.get());
+      } else {
+        notification.get().set(null);
+      }
+      notification = Optional.empty();
+    }
+  }
+}
