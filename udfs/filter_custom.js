381aed240d55b6dc6e7c985018da2d94d6270857
Prateek Duble <prateek.duble@gmail.com>
2025-06-12 13:43:13 -0400

    More UDFs

--- udfs/filter_custom.js
+++ udfs/filter_custom.js
@@ -1,10 +1,22 @@
+/*
+ * This UDF is used to filter messages based on a custom condition.
+ * 
+ * @param {Object} message - The message to filter.
+ * @param {Object} metadata - The metadata of the message.
+ * @returns {Object} The message if it meets the custom condition, otherwise null.
+ */
 function filter_custom(message, metadata) {
-
-    region = extractField(message, "region");
+    // Parse the message
+    const data = JSON.parse(message.data);
     
-    if (region === "US") {
-      return message;
+    // Extract region from the message
+    const region = data['region'];
+    
+    // Filter out messages that are not from US region
+    if (region !== "US") {
+        return null; // Return null to filter out the message
     }
     
-    return null;
-  }
+    // Return the original message if region is US
+    return message;
+}
