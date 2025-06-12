df4b2cd7c12c798cd36629f9066b69736184b1c0
Prateek Duble <prateek.duble@gmail.com>
2025-06-12 13:27:46 -0400

    More UDFs

---
+++ udfs/url_decode.js
@@ -0,0 +1,21 @@
+/*
+ * This UDF is used to URL decode specific fields within a message.
+ * 
+ * @param {Object} message - The message containing fields to URL decode.
+ * @param {Object} metadata - The metadata of the message.
+ * @returns {Object} The message with URL decoded fields.
+ */
+function url_decode(message, metadata) {
+    // Parse the message
+    const data = JSON.parse(message.data);
+    
+    // URL decode the specified field
+    if (data['url'] !== undefined) {
+        data['url'] = decodeURIComponent(data['url']);
+    }
+    
+    // Update the message with decoded data
+    message.data = JSON.stringify(data);
+    
+    return message;
+}
